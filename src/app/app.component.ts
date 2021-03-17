9805487206c6ae89961b9d015ff76b9333fda512
Jorge Espinosa Jiménez <jorgeespinosajimenez@MacBook-Pro-de-Jorge.local>
2021-03-17 17:54:11 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
-
+import { Component, Renderer2 } from '@angular/core';
+import { faGithub, faTwitter, faLinkedin, faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faGlobeEurope, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'minibio';
+  title = 'Linkinbio';
+
+  faGithub = faGithub;
+  faTwitter = faTwitter;
+
+  moon = faMoon;
+  sun= faSun;
+
+  isNightMode= false;
+
+  socialNetworks=[
+    {
+      image: faLinkedin,
+      titulo: "¡Contáctame por Linkedin!",
+      link:"https://www.linkedin.com/in/jorge-espinosa-jiménez-4aa622134/"
+
+    },
+    {
+      image: faGlobeEurope,
+      titulo: "¡Descarga mi CV en mi nueva Web!",
+      link:"https://jorgeespinosa.es"
+
+    },
+    {
+      image: faInstagram,
+      titulo: "¡Sigueme en Instagram!",
+      link:"https://www.instagram.com/ejcreativestudio/"
+
+    },
+    {
+      image: faYoutube,
+      titulo: "¡Sigueme en YouTube!",
+      link:"https://www.youtube.com/channel/UCyXnqdnY2sVEFyY8jkYauAg"
+
+    },
+
+    /* {
+      image: faGithub,
+      titulo: "¡Mis proyectos de GITHUB!",
+      link:"http://github.com"
+    }, */
+  ]
+
+  constructor ( private renderer: Renderer2){
+
+  }
+nightMode(){
+  this.isNightMode = !this.isNightMode
+  if(this.isNightMode) {
+    this.renderer.addClass(document.body, 'night');
+  } else {
+    this.renderer.removeClass(document.body, 'night');
+  }
+}
 }
